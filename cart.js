779a7cccae89d721ece412ece2204d122184ab77
pindilind <linda.gustafsson@outlook.com>
2021-02-23 09:45:17 +0100

    card styling cart and index

--- cart.js
+++ cart.js
@@ -5,45 +5,50 @@ function initCart() {
 
 function renderCart(cart) {
     document.getElementById("productCard").innerHTML=""
-
     
-    
+    /* if(!cart) {
+        let emptyCart = document.createElement(h3)
+        emptyCart.innerText = "Det finns inga produkter i varukorgen"
+        productCard.appendChild(emptyCart)
+    }
+     */
     cart.cartitems.forEach((cartItem => {
       cartItem.product.forEach(value => {
           
         let renderCardCart = document.createElement("div")
-        renderCardCart.className = "card", "mb-3", "align-items-center";
-
-        let rowDiv = document.createElement("div")
-        rowDiv.classList.add("row", "g-0")
-
-        let colDiv = document.createElement("div")
-        colDiv.classList.add("col","md-4")
+        renderCardCart.className = "card";
+        renderCardCart.style.width = "80%"
+        renderCardCart.style.height = "6rem"
+        renderCardCart.style.margin = "5px"
 
         let image = document.createElement("img")
         image.classList.add("card-img-top")
-        image.style.width = "100px" //Instead of a picture for now
-        image.style.height ="100px"//Instead of a picture for now
+        image.style.marginTop = "-25px"
+        image.style.width = "80px" //Instead of a picture for now
+        image.style.height = "80px"//Instead of a picture for now
         image.style.backgroundColor = "purple"//Instead of a picture for now
-        image.innerText="tomt på bilder just nu"
-
-        let colDivTwo = document.createElement("div");
-        colDivTwo.classList.add("col", "md-8")
-
+        image.innerText= "tomt på bilder just nu"
+        
         let cardBodyCart = document.createElement("div")
-        cardBodyCart.classList.add("card-body")
+        cardBodyCart.classList.add("card-body", "d-flex","flex-nowrap")
+        cardBodyCart.style.padding = "2rem"
 
         let title = document.createElement("h5");
-        title.classList.add("card-title")
-        title.innerText = value.name //placera productens namn här från databasen
+        title.classList.add("card-title","d-flex","p-0")
+        title.innerText = value.name + "  " //placera productens namn här från databasen
 
         let cardText = document.createElement("p")
-        cardText.classList.add("card-text")
-        cardText.innerText = value.price + " kr"//placera productens beskrivning här från databasen
+        cardText.classList.add("card-text","d-flex", "p-0")
+        cardText.innerText = " Pris: " + value.price + " kr " + "  " //placera productens beskrivning här från databasen
         
+
         let cardWeight = document.createElement("p")
-        cardWeight.classList.add("card-text",)
-        cardWeight.innerText = value.weight + " g"//placera productens beskrivning här från databasen
+        cardWeight.classList.add("card-text","d-flex","p-0")
+        cardWeight.innerText = " Vikt: " + value.weight + " g "//placera productens beskrivning här från databasen
+        
+        let cardQuant = document.createElement("p")
+        cardQuant.classList.add("card-text","d-flex","p-0")
+        cardQuant.innerText = " Antal: " + value.weight + " st"
 
        /*  let addbutton = document.createElement("button")
         addbutton.innerText = "Lägg i varukorgen"
@@ -53,11 +58,12 @@ function renderCart(cart) {
         addbutton.addEventListener("click", addProductToCart) /* () =>{
             alert ("you clicked the button");
     }); */ 
-    
+
     cardBodyCart.append(image)
     cardBodyCart.append(title)
     cardBodyCart.append(cardText)
     cardBodyCart.append(cardWeight)
+    cardBodyCart.append(cardQuant)
     /* cardBody.append(addbutton) */
     renderCardCart.append(cardBodyCart) // the body of content appends here
 
