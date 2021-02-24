1f7bdb7f5e88bdef6e3e33474fa69a150dc7916f
rusty-nick <YG91putty>
2021-02-24 14:09:18 +0100

    update niklas #79

--- api/repositories/productRepository.php
+++ api/repositories/productRepository.php
@@ -29,8 +29,10 @@ function addProduct($product){
     return $db->runQuery("INSERT INTO cartitem (userID,prodID, quantity) VALUES ( :userID, :prodID, :quantity);", $product);
 }
 function addOneQunatity($product){
+    $userID = $product["userID"];
+    $prodID = $product["prodID"];
     $db = new Database();
-    return $db->runQuery( "UPDATE cartitem SET quantity = quantity + 1 WHERE prodID = 'prodID'", $product);
+    return $db->fetchQuery( "UPDATE cartitem SET quantity = quantity + 1 WHERE prodID = $prodID AND userID = $userID");
 }
 /* ;INSERT INTO product (productname, price, weight, type) VALUES (:productname, :price, :weight, :type) */
 function getcartItem($userID){
